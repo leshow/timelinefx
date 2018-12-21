227a3890b2e122246a20876fa9caf770b51375b6
Evan Cameron <evan@vectorface.com>
2018-12-21 14:28:36 -0500

    Begin converting to class syntax

--- src/Matrix2.js
+++ src/Matrix2.js
@@ -1,54 +1,49 @@
+class Matrix2 {
+  constructor() {
+    this.set(1, 0, 0, 1);
+  }
 
-var Matrix2 = Class({
-  constructor: function() {
-    this.Set(1,0,0,1);
-  },
+  create(aa_ = 1, ab_ = 0, ba_ = 0, bb_ = 1) {
+    let m = new Matrix2();
+    m.set(aa_, ab_, ba_, bb_);
+    return m;
+  }
 
-  Create:function( aa_ /*= 1.0*/, ab_ /*= 0*/, ba_ /*= 0*/, bb_ /*= 1.0*/ )
-  {
-      var m = new Matrix2();
-      m.Set(aa_,ab_,ba_,bb_);
-      return m;
-  },
+  set(aa_ = 1, ab_ = 0, ba_ = 0, bb_ = 1) {
+    this.aa = aa_;
+    this.ab = ab_;
+    this.ba = ba_;
+    this.bb = bb_;
+  }
 
-  Set:function( aa_ /*= 1.0*/, ab_ /*= 0*/, ba_ /*= 0*/, bb_ /*= 1.0*/ )
-  {
-      this.aa = GetDefaultArg(aa_,1);
-      this.ab = GetDefaultArg(ab_,0);
-      this.ba = GetDefaultArg(ba_,0);
-      this.bb = GetDefaultArg(bb_,1);
-  },
+  scale(s) {
+    this.aa *= s;
+    this.ab *= s;
+    this.ba *= s;
+    this.bb *= s;
+  }
 
-  Scale:function( s )
-  {
-      this.aa *= s;
-      this.ab *= s;
-      this.ba *= s;
-      this.bb *= s;
-  },
-  Transpose:function( s )
-  {
-    var abt = this.ab;
+  transpose(s) {
+    let abt = this.ab;
     this.ab = this.ba;
     this.ba = abt;
-  },
+  }
 
-  TransformSelf:function( m )
-  {
-      var r_aa = this.aa * m.aa + this.ab * m.ba;
-      var r_ab = this.aa * m.ab + this.ab * m.bb;
-      var r_ba = this.ba * m.aa + this.bb * m.ba;
-      var r_bb = this.ba * m.ab + this.bb * m.bb;
+  transformSelf(m) {
+    let r_aa = this.aa * m.aa + this.ab * m.ba;
+    let r_ab = this.aa * m.ab + this.ab * m.bb;
+    let r_ba = this.ba * m.aa + this.bb * m.ba;
+    let r_bb = this.ba * m.ab + this.bb * m.bb;
 
-      this.Set(r_aa,r_ab,r_ba,r_bb);
-  },
+    this.setMatrix(r_aa, r_ab, r_ba, r_bb);
+  }
 
-  TransformVector:function( x, y )
-  {
-      var tv = new Vector2();
-      tv.x = x * this.aa + y * this.ba;
-      tv.y = x * this.ab + y * this.bb;
-      return tv;
-  },
+  transformVector(x, y) {
+    let tv = new Vector2();
+    tv.x = x * this.aa + y * this.ba;
+    tv.y = x * this.ab + y * this.bb;
+    return tv;
+  }
+}
 
-});
+export default Matrix2;
